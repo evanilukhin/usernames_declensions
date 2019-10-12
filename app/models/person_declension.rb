5eef8eb6c3dc8ee8010b9b1f9934b027fe066353
Evan Ilukhin <evanilukhin@gmail.com>
2019-10-12 13:00:59 +0300

    Add tests for declensions

--- app/models/person_declension.rb
+++ app/models/person_declension.rb
@@ -1,5 +1,5 @@
 class PersonDeclension < ApplicationRecord
   CASES = %w[genitive dative accusative instrumental prepositional].freeze
+
   belongs_to :person
-  
 end
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -2,8 +2,32 @@ describe 'Person' do
   context 'create' do
     let(:person) { create(:person, first_name: 'Илья', last_name: 'Ильясов', middle_name: 'Михайлович', sex: 'male') }
 
-    it 'initialize correct full_name' do
+    it 'initializes correct full_name' do
       expect(person.full_name).to eq('Ильясов Илья Михайлович')
+    end
+
+    it 'creates 6 declensions' do
+      expect(person.declensions.count).to eq(5)
+    end
+  end
+
+  context 'update' do
+    let(:person) { create(:person, first_name: 'Сергей', last_name: 'Ким', middle_name: 'Анатольевич', sex: 'male') }
+
+    before(:each) do
+      person.update(first_name: 'Иван')
+    end
+
+    it 'change full name' do
+      expect(person.full_name).to eq('Ким Иван Анатольевич')
+    end
+
+    it 'does not change declensions count' do
+      expect(person.declensions.count).to eq(5)
+    end
+
+    it 'change declensions' do
+      expect(person.declensions.pluck(:first_name)).to contain_exactly('Ивана', 'Ивану', 'Иваном', 'Иване', 'Ивана')
     end
   end
 end
