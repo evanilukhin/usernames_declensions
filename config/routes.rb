61238cadbe29b47d0ea4876534bc5e8502abd2c1
Evan Ilukhin <evanilukhin@gmail.com>
2019-10-13 09:30:46 +0300

    Add template for SPA Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get '/', to: 'index#index'
+  root 'index#index'
 
   namespace :api, constraints: { format: 'json' } do
     namespace :v1 do
       resources :people
     end
   end
+
+  match '*path', to: 'index#index', via: :all
 end
